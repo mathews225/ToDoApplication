639dfb491a9c53e46ae6df0431d836044d27fa84
mathews225 <petemathews14@gmail.com>
2021-03-02 11:16:36 -0500

    EntityFrameworkCore Design, Tools, SqlServer

--- ToDoApplication/Program.cs
+++ ToDoApplication/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
 
+#region SetUp
+/* 1. Add ToDoLibrary
+ * 2. Link App to Library
+ * 3. Add Tools and SqlServer to Lib and Design to App
+ *
+ */ 
+#endregion
+
 namespace ToDoApplication {
 	class Program {
 		static void Main(string[] args) {
+
+			
 			Console.WriteLine("Hello World!");
+
+			
 		}
 	}
 }
