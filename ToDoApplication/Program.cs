c19ecdb563aa84e15fde50dc127f66c052d00125
Pete Mathews <petemathews14@gmail.com>
2021-03-03 08:01:15 -0500

    small edits

--- ToDoApplication/Program.cs
+++ ToDoApplication/Program.cs
@@ -1,35 +1,41 @@
 ﻿using System;
+using ToDoLibrary.Controllers;
+using ToDoLibrary.Models;
 
 #region SetUp
 /* 1. Add ToDoLibrary
  * 2. Link App to Library
  * 3. Add Tools and SqlServer to Lib and Design to App
  *
- */ 
+ */
 #endregion
 
 namespace ToDoApplication {
 	class Program {
 
-
+		/*
 		void ListAllToDos() {
 			Cli.DisplayLine("Called ListAllToDos()");
 		}
+		*/
+
 
 
 		void Run() {
+
+			var todoCtrl = new TodosController();
+
 			Cli.DisplayLine("ToDo Application");
 			var option = DisplayMenu();
 			while (option != 0) {
 				switch (option) {
 					case 1:
-						ListAllToDos();
-						break;
+					break;
 					case 0:
-						return;
+					return;
 					default:
-						Cli.DisplayLine("Invalid option");
-						break;
+					Cli.DisplayLine("Invalid option");
+					break;
 				}
 				option = DisplayMenu();
 			}
--- ToDoLibrary/Controllers/TodosController.cs
+++ ToDoLibrary/Controllers/TodosController.cs
@@ -9,7 +9,7 @@ namespace ToDoLibrary.Controllers {
 	public class TodosController {
 		private readonly TodoDbContext _context;
 
-		public async Task<IEnumerable<Todo>> GetAll() {
+		public async Task<IEnumerable<Todo>> ListAllToDos() {
 			return await _context.Todos.ToListAsync();
 		}
 
@@ -17,8 +17,53 @@ namespace ToDoLibrary.Controllers {
 			return await _context.Todos.FindAsync(id);
 		}
 
+		public async Task<Todo> Create(Todo todo) {
+			if (todo == null) {
+				throw new Exception("ERROR: todo cannot be null!");
+			}
+			if (todo.Id != 0) {
+				throw new Exception("ERROR: todo.Id must be 0!");
+			}
+			_context.Todos.Add(todo);
+			var rowAffected = await _context.SaveChangesAsync();
+			if (rowAffected != 1) {
+				throw new Exception("ERROR: todo not created");
+			}
+			return todo;
+		}
+
+		public async Task Update(Todo todo) {
+			if (todo == null) {
+				throw new Exception("ERROR: todo cannot be null!");
+			}
+			if (todo.Id <= 0) {
+				throw new Exception("ERROR: todo.Id must be greater than 0!");
+			}
+			_context.Entry(todo).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+			var rowAffected = await _context.SaveChangesAsync();
+			if (rowAffected != 1) {
+				throw new Exception("ERROR: change failed!");
+			}
+			return;
+		}
+
+		public async Task<Todo> Delete(int id) {
+			var todo = _context.Todos.Find(id);
+			if (todo == null) {
+				return null;
+			}
+			_context.Todos.Remove(todo);
+			var rowsAffected = await _context.SaveChangesAsync();
+			if (rowsAffected != 1) {
+				throw new Exception("ERROR: Remove Failed");
+			}
+			Console.WriteLine($"ToDo DELETED!");
+			return todo;
+		}
+
 		public TodosController() {
 			_context = new TodoDbContext();
 		}
+
 	}
 }
